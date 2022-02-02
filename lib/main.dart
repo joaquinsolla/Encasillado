f2db8e40003cd95f32cfcf7bc4dfb381ec4e8275
joa <joaquin.solla@udc.es>
2022-02-02 12:34:09 +0100

    somes changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 import 'word_generator.dart';
 import 'init_view.dart';
 
+
 void main() {
+  /** RUN APP WITH ADITIONAL ARGS:
+   *  --no-sound-null-safety */
   generateWord();
   runApp(MyApp());
 }
