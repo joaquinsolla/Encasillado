6adc0f4eeffcb5356b9825cba42a8acf6458fa26
Joaquin <joaquin.solla@udc.es>
2022-03-25 12:46:33 +0100

    Working on user_stats.dart

--- lib/pages/user_stats.dart
+++ lib/pages/user_stats.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'dart:math';
 
 import 'package:Encasillado/common/miscellaneous.dart';
 import 'package:Encasillado/common/widgets.dart';
@@ -53,6 +54,24 @@ class _UserStatsState extends State<UserStats> {
       DeviceOrientation.portraitUp,
     ]);
 
+    int wotdHits = totalWotdGames-defeatsAtWotd;
+    String wotdPercentage = '0';
+    String wotdProm = '-';
+    if (wotdHits > 0) {
+      wotdPercentage = ((wotdHits/totalWotdGames)*100).toStringAsFixed(1);
+      wotdProm = [winsAtFirstWotd,winsAtSecondWotd,winsAtThirdWotd,winsAtFourthWotd,winsAtFifthWotd,winsAtSixthWotd].reduce(max).toString();
+    }
+
+    int infiniteHits = totalInfiniteGames-defeatsAtInfinite;
+    String infinitePercentage = '0';
+    String infiniteProm = '-';
+    String infiniteScoreRate = '-';
+    if (infiniteHits > 0) {
+      infinitePercentage = ((infiniteHits/totalInfiniteGames)*100).toStringAsFixed(1);
+      infiniteProm = [winsAtFirstInfinite,winsAtSecondInfinite,winsAtThirdInfinite,winsAtFourthInfinite,winsAtFifthInfinite,winsAtSixthInfinite].reduce(max).toString();
+    }
+    if(totalInfiniteGames > 0) infiniteScoreRate = (infiniteScore/totalInfiniteGames).toStringAsFixed(0);
+
     return Scaffold(
         appBar: myAppBarWithoutButtonsWithBackArrow(context),
         backgroundColor: appWhite,
@@ -64,7 +83,106 @@ class _UserStatsState extends State<UserStats> {
               child: ListView(
                 addAutomaticKeepAlives: true,
                 children: [
-                  Text('user_stats'),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    "Tus estadísticas",
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: appBlack,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.none,
+                      fontFamily: 'RaleWay',
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    "La palabra del día",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: appBlack,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.none,
+                      fontFamily: 'RaleWay',
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                  SizedBox(height: 2.5,),
+                  Text(
+                        "Partidas totales: $totalWotdGames           Prct. victoria: $wotdPercentage%\n"
+                            "Victorias: $wotdHits                        Intento promedio: $wotdProm\n"
+                            "Derrotas: $defeatsAtWotd",
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: appBlack,
+                          fontWeight: FontWeight.normal,
+                          decoration: TextDecoration.none,
+                          fontFamily: 'RaleWay',
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+
+                  //TODO: Insert wotd diagram
+
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    "Palabras infinitas",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: appBlack,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.none,
+                      fontFamily: 'RaleWay',
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                  SizedBox(height: 2.5,),
+                  Text(
+                    "Partidas totales: $totalInfiniteGames           Prct. victoria: $infinitePercentage%\n"
+                        "Victorias: $infiniteHits                        Intento promedio: $infiniteProm\n"
+                        "Derrotas: $defeatsAtInfinite                        Tasa puntos: $infiniteScoreRate pts/p",
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: appBlack,
+                      fontWeight: FontWeight.normal,
+                      decoration: TextDecoration.none,
+                      fontFamily: 'RaleWay',
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+
+                  //TODO: Insert infinite diagram
+
+                  Text(
+                    "\n*Prct.: Porcentaje\n* pts/p: Puntos por partida",
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: appGrey,
+                      fontWeight: FontWeight.normal,
+                      decoration: TextDecoration.none,
+                      fontFamily: 'RaleWay',
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                  SizedBox(height: 5,),
+                  Text(
+                    "\nGracias por jugar a Encasillado v$appVersion",
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: appGrey,
+                      fontWeight: FontWeight.normal,
+                      decoration: TextDecoration.none,
+                      fontFamily: 'RaleWay',
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 20,),
                 ],
               ),
             ),
